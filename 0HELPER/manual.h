d683fa37e6466f6e9364488359374b942726691c
Lucas Ahlsweh <lahlsweh@k1r4p10.42mulhouse.fr>
2025-06-24 13:23:35 +0200

    rework manual.h

--- 0HELPER/manual.h
+++ 0HELPER/manual.h
@@ -1,136 +1,173 @@
-
-
-typedef uint32_t		socklen_t;
-typedef long			off_t;
-typedef unsigned long	nfds_t;
-typedef uint32_t		in_addr_t;
+typedef unsigned long		size_t;
+typedef unsigned long long	ssize_t;
+typedef unsigned int		socklen_t;
 
 typedef struct	sockaddr
 {
-	sa_family_t	sa_family;   // Address family (e.g., AF_INET for IPv4)
-	char		sa_data[14]; // Socket address data
-};
-
-typedef struct	sigaction
-{
-	void		(*sa_handler)(int);                        // Signal handler function
-	void		(*sa_sigaction)(int, siginfo_t *, void *); // Signal handler function with additional info
-	sigset_t	sa_mask;   // Additional set of signals to be blocked during the execution of the signal handler
-	int			sa_flags;                                  // Special flags to affect the behavior of the signal
-};
-
-typedef struct	protoent
-{
-	char	*p_name;     // Official name of the protocol
-	char	**p_aliases; // Alias list for the protocol
-	int		p_proto;     // Protocol number
-};
-
-typedef struct	hostent
-{
-	char	*h_name;	   // Official name of the host
-	char	**h_aliases;   // Alias list for the host
-	int		h_addrtype;    // Host address type (e.g., AF_INET)
-	int		h_length;      // Length of the address
-	char	**h_addr_list; // List of addresses for the host
-};
-
-typedef struct	stat
-{
-	dev_t		st_dev;      // ID of device containing file
-	ino_t		st_ino;      // Inode number
-	mode_t		st_mode;     // File type and mode
-	nlink_t		st_nlink;    // Number of hard links
-	uid_t		st_uid;	     // User ID of owner
-	gid_t		st_gid;	     // Group ID of owner
-	dev_t		st_rdev;     // Device ID (if special file)
-	off_t		st_size;     // Total size, in bytes
-	blksize_t	st_blksize;  // Block size for filesystem I/O
-	blkcnt_t	st_blocks;   // Number of 512B blocks allocated
-	time_t		st_atime;    // Time of last access
-	time_t		st_mtime;    // Time of last modification
-	time_t		st_ctime;    // Time of last status change
-};
-
-typedef struct	pollfd
-{
-	int		fd;	     // File descriptor
-	short	events;  // Requested events to monitor
-	short	revents; // Returned events that occurred
-};
-
-typedef struct	addrinfo
-{
-	int				ai_flags;      // Input flags
-	int				ai_family;     // Address family for socket
-	int				ai_socktype;   // Socket type
-	int				ai_protocol;   // Protocol for socket
-	socklen_t		ai_addrlen;    // Length of socket address
-	struct sockaddr	*ai_addr;      // Socket address for socket
-	char			*ai_canonname; // Canonical name for service location
-	struct addrinfo	*ai_next;      // Pointer to next structure in linked list
-};
-
-typedef struct	in_addr
-{
-	in_addr_t	s_addr; // IPv4 address
+	unsigned short	sa_family;		// Common data: address family and length.
+	char			sa_data[14];	// Address data.
 };
 
 #include <sys/socket.h>
+
 int				socket(int domain, int type, int protocol);
-int				setsockopt(int socket, int level, int option_name, const void *option_value, socklen_t option_len);
-int				getsockname(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len);
-int				bind(int socket, const struct sockaddr *address, socklen_t address_len);
-int				connect(int socket, const struct sockaddr *address, socklen_t address_len);
+int				setsockopt(int socket, int level, int option_name, const void* option_value, socklen_t option_len);
+int				getsockname(int socket, struct sockaddr* address, socklen_t* address_len);
+int				getsockname(int sockfd, struct sockaddr* addr, socklen_t* addrlen);
+int				bind(int socket, const struct sockaddr* address, socklen_t address_len);
+int				connect(int socket, const struct sockaddr* address, socklen_t address_len);
 int				listen(int socket, int backlog);
-int				accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len);
-ssize_t			send(int socket, const void *buffer, size_t length, int flags);
-ssize_t			recv(int socket, void *buffer, size_t length, int flags);
+int				accept(int socket, struct sockaddr* address, socklen_t* address_len);
+ssize_t			send(int socket, const void* buffer, size_t length, int flags);
+ssize_t			recv(int socket, void* buffer, size_t length, int flags);
+
+/*---------------------------------------------------------------------------------------------------------------*/
 
 #include <arpa/inet.h>
+
 uint32_t		htonl(uint32_t hostlong);
 uint16_t		htons(uint16_t hostshort);
 uint32_t		ntohl(uint32_t netlong);
 uint16_t		ntohs(uint16_t netshort);
 
+/*---------------------------------------------------------------------------------------------------------------*/
+
 #include <signal.h>
+
 void			(*signal(int sig, void (*func)(int)))(int);
-int				sigaction(int sig, const struct sigaction *restrict act, struct sigaction *restrict oact);
+int				sigaction(int sig, const struct sigaction* act, struct sigaction* oact);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+typedef char*	h_addr;
+
+// Description of data base entry for a single service.
+typedef struct	protoent
+{
+	char*	p_name;		// Official protocol name.
+	char**	p_aliases;	// Alias list.
+	int		p_proto;		// Protocol number.
+};
+
+// Description of data base entry for a single host.
+typedef struct	hostent
+{
+  char*		h_name;		// Official name of host.
+  char**	h_aliases;	// Alias list.
+  int 		h_addrtype;		// Host address type.
+  int 		h_length;		// Length of address.
+  char**	h_addr_list;	// List of addresses from name server.
+  h_addr	h_addr_list[0];	// Address, for backward compatibility.
+};
 
 #include <netdb.h>
-struct protoent	*getprotobyname(const char *name);
-struct hostent	*gethostbyname(const char *name);
+
+struct protoent*	getprotobyname(const char* name);
+struct hostent*		gethostbyname(const char* name);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+typedef long	off_t;
 
 #include <unistd.h>
+
 int				close(int fildes);
 off_t			lseek(int fildes, off_t offset, int whence);
 
+/*---------------------------------------------------------------------------------------------------------------*/
+
+typedef struct stat
+{
+	unsigned long	st_dev;				// Device.
+	unsigned long	st_ino;				// File serial number.
+	unsigned long	st_nlink;			// Link count.
+	unsigned int	st_mode;			// File mode.
+	unsigned int	st_uid;				// User ID of the file's owner.
+	unsigned int	st_gid;				// Group ID of the file's group.
+	unsigned long	st_rdev;			// Device number, if device.
+	long			st_size;			// Size of file, in bytes.
+	long			st_blksize;			// Optimal block size for I/O.
+	long			st_blocks;			// Number 512-byte blocks allocated.
+	long			st_atime;			// Time of last access.
+    unsigned long	st_atimensec;		// Nscecs of last access.
+    long			st_mtime;			// Time of last modification.
+    unsigned long	st_mtimensec;		// Nsecs of last modification.
+    long			st_ctime;			// Time of last status change.
+    long			st_ctimensec;		// Nsecs of last status change.
+	long			__glibc_reserved[3];
+};
+
 #include <sys/stat.h>
-int				fstat(int fildes, struct stat *buf);
+
+int				fstat(int fildes, struct stat* buf);
+
+/*---------------------------------------------------------------------------------------------------------------*/
 
 #include <fcntl.h>
+
 int				fcntl(int fildes, int cmd, ...);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+// Type used for the number of file descriptors.
+typedef unsigned long int	nfds_t;
+
+// Data structure describing a polling request.
+typedef struct	pollfd
+{
+	int			fd;			// File descriptor to poll.
+	short int	events;		// Types of events poller cares about.
+	short int	revents;	// Types of events that actually occurred.
+};
 
 #include <poll.h>
 int				poll(struct pollfd fds[], nfds_t nfds, int timeout);
 
+/*---------------------------------------------------------------------------------------------------------------*/
+
+typedef unsigned int		socklen_t;
+
+typedef struct	sockaddr
+{
+	unsigned short	sa_family;		// Common data: address family and length.
+	char			sa_data[14];	// Address data.
+};
+
+// Structure to contain information about address of a service provider.
+struct addrinfo
+{
+	int					ai_flags;		// Input flags.
+	int					ai_family;		// Protocol family for socket.
+	int					ai_socktype;	// Socket type.
+	int					ai_protocol;	// Protocol for socket.
+	socklen_t			ai_addrlen;		// Length of socket address.
+	struct sockaddr*	ai_addr;		// Socket address for socket.
+	char*				ai_canonname;	// Canonical name for service location.
+	struct addrinfo*	ai_next;		// Pointer to next in list.
+};
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-int				getaddrinfo(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res);
-void			freeaddrinfo(struct addrinfo *res);
+
+int				getaddrinfo(const char* node, const char* service, const struct addrinfo* hints, struct addrinfo** res);
+void			freeaddrinfo(struct addrinfo* res);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+// Internet address.
+typedef uint32_t	in_addr_t;
+
+struct in_addr
+{
+	in_addr_t	s_addr;
+};
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-in_addr_t		inet_addr(const char *cp);
-char			*inet_ntoa(struct in_addr in);
 
+in_addr_t		inet_addr(const char* cp);
+char*			inet_ntoa(struct in_addr in);
 
-/*
-Create a socket
-Bind it to a port (like 6667)
-Listen for connections
-Accept a connection
-Receive data and send replies
-*/
+/*---------------------------------------------------------------------------------------------------------------*/
--- 2TEST/main.cpp
+++ 2TEST/main.cpp
@@ -6,17 +6,28 @@
 /*   By: lahlsweh <lahlsweh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 15:15:22 by lahlsweh          #+#    #+#             */
-/*   Updated: 2025/06/23 16:13:22 by lahlsweh         ###   ########.fr       */
+/*   Updated: 2025/06/24 12:52:02 by lahlsweh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*  printf() : <stdio.h>
+    memset() : <string.h>
+     close() : <unistd.h>
+     htons() : <arpa/inet.h>
+    socket() : <sys/socket.h>
+setsockopt() : <sys/socket.h>
+      bind() : <sys/socket.h>
+    listen() : <sys/socket.h>
+    accept() : <sys/socket.h>
+      recv() : <sys/socket.h>
+	  poll() : <poll.h>*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <poll.h>
 
 #define PORT 6667
 #define BUFFER_SIZE 1024
@@ -25,27 +36,27 @@ int	main(void)
 {
 	struct sockaddr_in	address;
 	char				buffer[BUFFER_SIZE] = {0};
-	int					server_fd;
-	int					new_socket;
+	int					fd_server_socket;
+	int					fd_client_socket;
 	int					opt = 1;
 	int					addrlen = sizeof(address);
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 	address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-	bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-	listen(server_fd, 3);
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+	fd_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	setsockopt(fd_server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	bind(fd_server_socket, (struct sockaddr *)&address, sizeof(address));
+	listen(fd_server_socket, 3);
 	printf("Server listening on port %d...\n", PORT);
 	while (1)
 	{
-		new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+		fd_client_socket = accept(fd_server_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 		printf("Client connected.\n");
 		while (1)
 		{
 			memset(buffer, 0, BUFFER_SIZE);
-			ssize_t bytes_read = recv(new_socket, buffer, BUFFER_SIZE, 0);
+			ssize_t bytes_read = recv(fd_client_socket, buffer, BUFFER_SIZE, 0);
 			if (bytes_read <= 0)
 			{
 				printf("Client disconnected.\n");
@@ -54,7 +65,7 @@ int	main(void)
 			printf("Received: %s", buffer);
 		}
 	}
-	close(new_socket);
-	close(server_fd);
+	close(fd_client_socket);
+	close(fd_server_socket);
 	return (0);
 }
