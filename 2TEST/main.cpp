131164587a063fc37af2e2b23ea9c6142a25ebb1
Lucas Ahlsweh <lahlsweh@k1r4p10.42mulhouse.fr>
2025-06-24 16:36:46 +0200

    update manual.h && 2TEST

--- 2TEST/main.cpp
+++ 2TEST/main.cpp
@@ -5,63 +5,118 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lahlsweh <lahlsweh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/23 15:15:22 by lahlsweh          #+#    #+#             */
-/*   Updated: 2025/06/24 12:52:02 by lahlsweh         ###   ########.fr       */
+/*   Created: 2025/06/24 16:34:07 by lahlsweh          #+#    #+#             */
+/*   Updated: 2025/06/24 16:36:10 by lahlsweh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*  printf() : <stdio.h>
-    memset() : <string.h>
-     close() : <unistd.h>
-     htons() : <arpa/inet.h>
-    socket() : <sys/socket.h>
+	memset() : <string.h>
+	 close() : <unistd.h>
+	 htons() : <arpa/inet.h>
+	socket() : <sys/socket.h>
 setsockopt() : <sys/socket.h>
-      bind() : <sys/socket.h>
-    listen() : <sys/socket.h>
-    accept() : <sys/socket.h>
-      recv() : <sys/socket.h>
-	  poll() : <poll.h>*/
+	  bind() : <sys/socket.h>
+	listen() : <sys/socket.h>
+	accept() : <sys/socket.h>
+	  recv() : <sys/socket.h>*/
+
+/*typedef unsigned long socklen_t
+
+typedef struct in_addr { unsigned long	s_addr; };
+
+typedef struct	sockaddr
+{
+	unsigned short	sa_family;		// Common data: address family and length.
+	char			sa_data[14];	// Address data.
+};
+
+//Structure describing an Internet socket address.
+typedef struct sockaddr_in
+{
+	unsigned short	sin_family;
+	unsigned short	sin_port;	// Port number.
+	struct in_addr	sin_addr;	// Internet address.
+	// Pad to size of `struct sockaddr'.
+	unsigned char	sin_zero[sizeof(sockaddr) - sizeof(unsigned short) - sizeof(unsigned short) - sizeof(in_addr)];
+};*/
+
+/*
+AF_INET -> stands for Address Family: Internet (IPv4)
+
+htons(PORT) used because:
+Host byte order: The byte order your machine uses internally.
+	On many common machines like x86 (Intel/AMD), this is little-endian (Least Significant Byte first).
+Network byte order: The standardized byte order used in network protocols,
+	which is big-endian (Most Significant Byte first).
+
+INADDR_ANY -> "Address to accept any incoming messages"
+
+SOCK_STREAM -> "Sequenced, reliable, connection-based byte streams."
+	typically used in TCP connections, like our IRC server
+
+IPPROTO_TCP -> "Transmission Control Protocol." (for TCP/IPv4)
+
+NOTE : setsockopt() can only set one option at a time !
+the __level parameter tells the system which protocol layer the option belongs to
+the __optname parameter indicates the options applied to said __level
+level  : SOL_SOCKET -> stands for "socket-level" option level
+option : SO_REUSEADDR -> Allows immediate reuse of a port after program exit
+option : SO_KEEPALIVE -> Sends keepalive packets on TCP connections to detect dead clients
+
+bind() associates a socket with a specific IP address and port number
+
+memset() is used to properly empty and cleanup buffer before subsequent recv() calls. fills buffer data with '\0'
+
+recv() is used to receive data from client and assign it to buffer
+MSG_DONTWAIT -> Make the call non-blocking. useful for multi-client handling*/
 
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <poll.h>
+#include <errno.h>
 
 #define PORT 6667
 #define BUFFER_SIZE 1024
+#define QUEUE_SIZE 16 // Must be <= SOMAXCONN
 
 int	main(void)
 {
-	struct sockaddr_in	address;
-	char				buffer[BUFFER_SIZE] = {0};
+	struct sockaddr_in	IPV4_server_socket_address;
+	struct sockaddr_in	IPV4_client_socket_address;
 	int					fd_server_socket;
 	int					fd_client_socket;
-	int					opt = 1;
-	int					addrlen = sizeof(address);
+	char				client_ip4[INET_ADDRSTRLEN];
+	char				buffer[BUFFER_SIZE] = {0};
+	int					opt_toggle = 1;
+	socklen_t			client_addrlen = sizeof(IPV4_client_socket_address);
+	ssize_t				bytes_read;
 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-	fd_server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(fd_server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-	bind(fd_server_socket, (struct sockaddr *)&address, sizeof(address));
-	listen(fd_server_socket, 3);
+	IPV4_server_socket_address.sin_family = AF_INET;
+	IPV4_server_socket_address.sin_port = htons(PORT);
+	IPV4_server_socket_address.sin_addr.s_addr = INADDR_ANY;
+	fd_server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	setsockopt(fd_server_socket, SOL_SOCKET, SO_REUSEADDR, &opt_toggle, sizeof(opt_toggle));
+	setsockopt(fd_server_socket, SOL_SOCKET, SO_KEEPALIVE, &opt_toggle, sizeof(opt_toggle));
+	bind(fd_server_socket, (struct sockaddr *)&IPV4_server_socket_address, sizeof(IPV4_server_socket_address));
+	listen(fd_server_socket, QUEUE_SIZE);
 	printf("Server listening on port %d...\n", PORT);
+
 	while (1)
 	{
-		fd_client_socket = accept(fd_server_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-		printf("Client connected.\n");
-		while (1)
+		bytes_read = recv(fd_client_socket, buffer, (BUFFER_SIZE - 1), MSG_DONTWAIT);
+		if (bytes_read == 0) { printf("Client disconnected.\n"); break; }
+		else if (bytes_read < 0)
 		{
-			memset(buffer, 0, BUFFER_SIZE);
-			ssize_t bytes_read = recv(fd_client_socket, buffer, BUFFER_SIZE, 0);
-			if (bytes_read <= 0)
-			{
-				printf("Client disconnected.\n");
-				break ;
-			}
+			// No data available now, just continue (or maybe sleep a bit)
+			if (errno == EAGAIN || errno == EWOULDBLOCK) { continue ; }
+			else { perror("recv"); break ; }
+		}
+		else
+		{
+			buffer[bytes_read] = '\0';
 			printf("Received: %s", buffer);
 		}
 	}
