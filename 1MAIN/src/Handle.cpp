e6cd82fe8f4bb3ddfac37325d990561830f4cef6
Nicolas Dionis <nidionis@k2r4p4.42mulhouse.fr>
2025-07-20 18:48:12 +0200

    a skeleton of CAP REQ src/Handle.cpp

--- 1MAIN/src/Handle.cpp
+++ 1MAIN/src/Handle.cpp
@@ -39,32 +39,75 @@ std::string isValidName(const std::string& str) {
     throw (std::runtime_error("Invalid name"));
 }
 
+void multiPrefix(Server &server, Client &client, std::string input) {
+    (void) server;
+    (void) client;
+    client.send("from function multiPrefix\r\n");
+    client.send("returns prefixes set in macro USER_PREFIXES (Client.hpp)");
+    client.send("\r\n");
+    for (int i = 0; USER_PREFIXES[i]; i++) {
+        client.send(" ");
+        client.send(std::string(1, USER_PREFIXES[i]));
+    }
+    client.send("args " + input + "not managed\r\n");
+}
+
+static const struct s_cmd req[] = {
+        {"multi-prefix",    &multiPrefix },
+        {"",                NULL }
+};
+
+static void capLS(Server &server, Client &client, std::string args) {
+    (void) server;
+    (void) args;
+    client.send(" CAP * LS : ");
+    for (int i = 0; req[i].f; i++) {
+        client.send(" ");
+        client.send(req[i].header);
+    }
+    client.send("\nCAP END");
+    client.send("\n");
+}
+
+static void capREQ(Server &server, Client &client, std::string caps) {
+    (void) server;
+    std::string cap = getHead(caps);
+    std::cout << "[capREQ] caps:" << caps << std::endl;
+    while (!cap.empty()) {
+        for (int i = 0; req[i].f; i++) {
+            std::cout << "req[i].header: " << req[i].header << std::endl;
+            std::cout << "cap: " << cap << std::endl;
+            if (cap == req[i].header) {
+                req[i].f(server, client, "");
+                return;
+            }
+            caps = getNextWds(caps);
+            cap = getHead(caps);
+        }
+    }
+    client.send("caps");
+    client.send(": Invalid CAP command");
+}
+
 void cmdCap(Server &server, Client &client, std::string args) {
     (void) server;
-    //should wait NICK an USER
-    std::cout << "[cmdCap] args: " << args << "\n";
-        std::cout << "[cmdCap] getHead: " << getHead(args) << "\n";
+    std::cout << "[cmdCap] args:" << args << std::endl;
     if (getHead(args) == "LS") {
-        client.send("CAP * LS :");
-        client.send(" CAP * LS : https://ircv3.net/specs/extensions/capability-negotiation");
-        client.send("CAP END");
-        client.send("\n");;
+        capLS(server, client, getNextWds(args));
+    } else if (getHead(args) == "REQ") {
+        capREQ(server, client, getNextWds(args));
     }
-    // should wait a cap end
 }
 
 void cmdNick(Server &server, Client &client, std::string input) {
     (void) server;
     std::string nick = getHead(input);
     if (nick != "") {
-        if (client.getNickname() != "") {
-            client.send("NICK :Nickname already set\r\n"); return; }
         if (server.hasNick(nick))
-            client.send("NICK :Nickname already attributed\r\n");
+            client.send("NICK :NickName already in use\r\n");
         else
             client.setNickname(nick);
-    } else
-        client.send("NICK :You are now known as " + client.getNickname() + "\r\n");
+    }
 }
 
 void cmdUser(Server &server, Client &client, std::string input) {
@@ -72,12 +115,11 @@ void cmdUser(Server &server, Client &client, std::string input) {
     std::string user = getHead(input);
     if (input != "")
     {
-        if (client.getUsername() == "")
-            client.setUsername(user);
-        else
+        if (server.hasUser(user))
             client.send("USER :Username already in use\r\n");
+        else
+            client.setUsername(user);
     }
-    client.send("USER :You are now known as " + client.getUsername() + "\r\n");
 }
 
 void cmdJoin(Server &server, Client &client, std::string input) {
@@ -99,7 +141,7 @@ void cmdPrivmsg(Server &server, Client &client, std::string input) {
     // Handle PRIVMSG command
 }
 
-static const struct s_cmd commands[] = {
+static const struct s_cmd commands [] = {
         {"CAP",     &cmdCap },
         {"NICK",    &cmdNick},
         {"USER",    &cmdUser},
@@ -118,4 +160,5 @@ void processCommand(Server &server, Client &client, std::string input) {
             return;
         }
     }
+    client.send("[processCommand]: Invalid command\n");
 }
