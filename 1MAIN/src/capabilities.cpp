d9756a5ba118d901bd75a83b5aa516b123b264e6
Nicolas Dionis <nidionis@k2r4p9.42mulhouse.fr>
2025-07-29 13:55:21 +0200

    [not tested] CAP better implemented

--- 1MAIN/src/capabilities.cpp
+++ 1MAIN/src/capabilities.cpp
@@ -33,11 +33,11 @@ void multiPrefix(Server& server, Client& client, std::string input)
     (void)server;
     (void)client;
     (void)input;
-    if (client.hasCap("multi-prefix")) {
-        client.resetCap("multi-prefix");
-    } else {
+    //if (client.hasCap("multi-prefix")) {
+    //    client.resetCap("multi-prefix");
+    //} else {
         client.setCap("multi-prefix");
-    }
+    //}
 }
 
 void capLs(Server& server, Client& client, std::string args)
@@ -57,22 +57,21 @@ void capLs(Server& server, Client& client, std::string args)
 void capReq(Server& server, Client& client, std::string caps)
 {
     (void)server;
-    std::string cap = getHead(caps);
+    std::string cap = popWd(caps);
     std::cout << "[capReq] caps:" << caps << std::endl;
     //client.send(server.getName());
-    while (!cap.empty())
+    if (!cap.empty())
     {
         for (int i = 0; cap_tab[i].f; i++)
         {
             if (cap == cap_tab[i].header) {
                 cap_tab[i].f(server, client, "");
             } else {
-                client.send("\n[capReq] : " + cap + "Invalid\r\n");
+                client.send(":" + server.getName() + " CAP " + client.getNickname() + " NAK :" + cap + "\r\n");
             }
-            caps = getNextWds(caps);
-            cap = getHead(caps);
         }
     }
+    client.send(":" + server.getName() + " 461 " + client.getNickname() + " CAP :Not enough parameters\r\n");
 }
 
 static void server_banner(Client &client)
@@ -94,8 +93,10 @@ static void server_banner(Client &client)
 void capEnd(Server &server, Client &client, std::string caps) {
     (void)server;
     (void)caps;
-    if (client.getUsername() != "" && client.getNickname() != "" && client.hasFlag(PASSWD_OK)) {
-        client.setFlag(LOGGED);
+    if (!client.hasFlag(LOGGED)) {
+        if (client.getUsername() != "" && client.getNickname() != "" && client.hasFlag(PASSWD_OK)) {
+            client.setFlag(LOGGED);
+        }
+        server_banner(client);
     }
-    server_banner(client);
 }
